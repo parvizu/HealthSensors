bb1c5989789a7a3ce619f0ac48b6ea9daaa9e315
Pablo <parvizu@ischool.berkeley.edu>
2015-01-06 00:07:22 -0500

    Heart rate and physical activity mostly done.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,19 +1,29 @@
 var data, cleanData;
 var fields = ["airTemp","calories","date","epoch","gsr","heartrate","skinTemp","steps"]
 
+var userConfiguration = {
+	'week':{
+		'days':[],
+		'maxValue':0
+	},
+	'mainParameters': {}
+}
+
 var mainParameters = {
 	'heartrate': {
-		'h': 90,
-		'l': 60,
-		'interval' : 10, //in minutes
-		'showAnomalies': false
+		'h': 80,
+		'l': 55,
+		'showAnomalies': true
+	},
+	'general': {
+		'interval': 5 //in minutes
 	}
 }
 
 $(document).ready( function() {
 	data = loadData();
 	cleanData = cleanData(data['1'])
-	showHeartRateWeek();
+	// showHeartRateWeek();
 });
 
 function loadData() {
@@ -71,7 +81,7 @@ function createConfigFile(data,target,id,className,field) {
 			'top': 0,
 			'right': 0,
 			'bottom': 0,
-			'left': 5
+			'left': 0
 		},
 		day: getDateDay(data[0]),
 		date: new Date(data[0].date_human),
@@ -160,31 +170,52 @@ function getDataAnomalies(data) {
 
 /*
 	Simple function that analizes the date and returns the name of the day of the week.
-*/
+*/ 
 function getDateDay(date) {
 	var weekdays = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
 	var d = new Date(date['date_human']);
 	return weekdays[d.getDay()];
 }
 
-
 function setConfigData(data,field) {
 	var configData = []
-	$.each(data, function(i,d) {
-		//selects intervals of minutes (600 = 10 mins, 300 = 5 mins, etc...)
-		var time = new Date(d.date_human);
-		if (time.getMinutes()%mainParameters.heartrate.interval == 0) {
-			if (d[field] === 'None' || d.date_epoch =="") {	
-				configData.push("0");
+	var aggregate = 0;
+	if (field == 'steps') {
+		$.each(data, function(i,d) {
+			var time = new Date(d.date_human);
+			var minute = i+1;
+			if (d[field] !== 'None' || d.date_epoch !=="") {	
+				aggregate+=parseInt(d[field]);
+				// configData.push(aggregate);
+			}	
+
+			if (minute%mainParameters.general.interval == 0) {
+				configData.push(aggregate);
+				aggregate = 0;
+			}	
+			
+		});
+		console.log(configData);
+		return configData;
+	}
+	else {
+		$.each(data, function(i,d) {
+			//selects intervals of minutes (600 = 10 mins, 300 = 5 mins, etc...)
+			var time = new Date(d.date_human);
+			if (time.getMinutes()%mainParameters.general.interval == 0) {
+				if (d[field] === 'None' || d.date_epoch =="") {	
+					configData.push("0");	
+				}
+				else {
+					configData.push(d[field]);	
+				}
 			}
-			else {
-				configData.push(d[field]);
-			}
-		}
-	});
+		});
+	}
+	
+	console.log(configData);
 	return configData;
 }	
-
 
 function createLineChart(config) {
 	var margin = {
@@ -217,15 +248,7 @@ function createLineChart(config) {
 	    .x(function(d,i) { return x(i); })
 	    .y(function(d) { return y(d); });
 
-	var dayHeader = d3.select(config.target).append('div')
-			.attr("id",config.id)
-			.attr("class",config.className)
-		.append('div')
-			.attr("class","chartDayHeader")
-		.append('div')
-		
-	dayHeader.append('h3')
-		.text(config.day);
+	getDayHeadersForWeeklyCharts(config.target,config.id,config.className,config.day);
 		
 	// This would add the month and day underneath the day of the week	
 	// dayHeader.append('span')
@@ -278,7 +301,7 @@ function createLineChart(config) {
 				svg.append("rect")
 					.attr({
 						"x": function() { 
-							var ax = x(d[0].id/mainParameters.heartrate.interval); 
+							var ax = x(d[0].id/mainParameters.general.interval); 
 							return ax; 
 						},
 						"y": function() {
@@ -307,16 +330,11 @@ function createLineChart(config) {
 		addAnomaliesToChart(config.anomalies.high,"h");
 		addAnomaliesToChart(config.anomalies.low,"l");		
 	}
-
-
 }
 
-
-
-
-function getWeekData(field) {
+function getWeekData(field,start) {
 	var week = [];	 
-	var start = parseInt('1401951600');
+	var start = parseInt(start);
 	var end = start + 86400;
 	for (var i = 0; i<7; i++) {
 		week.push(getDayData(start,end,field));
@@ -326,9 +344,7 @@ function getWeekData(field) {
 	return week;
 }
 
-
 function getDayData(start,end, field) {
-	
 	var clean = {};
 	for (var i = 0; i<= 1439 ; i++) {
 		clean[i] = {
@@ -337,7 +353,7 @@ function getDayData(start,end, field) {
 			date_epoch: ''
 		};
 	}
-	var min;
+	var minday;
 	var day = [];
 	$.each(mainData, function(i,d) {
 		if (parseInt(d.date_epoch.trim()) >= start && parseInt(d.date_epoch.trim()) < end) {
@@ -357,34 +373,24 @@ function getDayData(start,end, field) {
 	return day;
 }
 
-
-function createWeekHeartRate() {
-	var week = getWeekData('heartrate');
-	var dayConfig;
-	$.each(week, function(i,d) {
-		dayConfig = createConfigFile(d,'#heartRate','hr'+i,'heartrate','heartrate');
-		createLineChart(dayConfig);
-	})
-}
-
-
-function buildTimeScale(data) {
+function buildTimeScale(target,data) {
 	var x = d3.scale.linear()
 		.domain([0,24])
-	    .range([5, 800]);
+	    .range([0, 800]);
 
 	var xAxis = d3.svg.axis()
 	    .scale(x)
 	    .ticks(data.length)
-	    .tickSize(10,1)
+	    .tickSize(10,1);
 
-	var svg = d3.select("#heartRate").append("div")
+	var svg = d3.select("#"+target).append("div")
 				.attr("class","scale")
 				.append("svg")
 					.attr("class", "axis")
 					.attr("class", "timeScale")
-					.attr("width", 790)
-					.attr("height", 65)
+					.attr("width", 810)
+					.attr("height", 65);
+
 	svg.append("g")
 		.attr("class","timeAxis")
 		.attr("transform", "translate(0,40)")
@@ -410,16 +416,157 @@ function buildTimeScale(data) {
 	var scaleLabels =["",1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
 	$.each($(".tick text"),function(i,d) {
 		$(d).html(scaleLabels[i]);
-	})
-
-	
+		// $(d).attr("x","-5");
+	});	
 }
 
 function showHeartRateWeek() {
+	$(".subsection").hide();
 	$("#heartRate").html('');
 	$("#heartRate").toggle();
 	
-	buildTimeScale([7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,1,2,3,4,5,6]);
-	createWeekHeartRate();
+	buildTimeScale('heartRate',[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]);
+	createWeekChart('heartRate','1401951600','hr');
 }
+
+
+/*
+	This function will create the weekley breakdown on a day by day basis of a particular activity passed as a parameter. 
+
+	asd
+*/
+function createWeekChart(category,start, shortname) {
+	var week = getWeekData(category.toLowerCase(),start);
+	var max = 0;
+	userConfiguration.week.days = [];
+	$.each(week, function(i,d) {
+		userConfiguration.week.days.push(createConfigFile(d,'#'+category,shortname+i,category.toLowerCase(),category.toLowerCase()));
+		max = getWeekMax(userConfiguration.week.days[i].data, max);
+	});
+	userConfiguration.week.maxValue = max;
+	
+	$.each(userConfiguration.week.days, function(i,config) {
+		if (shortname === 'pa') {
+			createBarChart(config);
+		}
+		else if (shortname === 'hr') {
+			createLineChart(config);	
+		}
+	})
+}
+
+function getWeekMax(data, current) {
+	var m = Math.max.apply(Math,data);
+	if (m> current)
+		return m;
+	else
+		return current;
+}
+
+/********* PHYSICAL ACTIVITY ***********/
+function showPhysicalActivityWeek() {
+	$(".subsection").hide();
+	$("#steps").html('');
+	// $("#steps").html('');
+	$("#steps").toggle('');
+	buildTimeScale('steps',[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]);
+	createWeekChart('steps','1401951600','pa');
+}
+
+function getDayHeadersForWeeklyCharts(target,id,className,day) {
+	var dayHeader = d3.select(target).append('div')
+			.attr("id",id)
+			// .attr("class",className)
+			.attr("class","dayChart " + className)
+		.append('div')
+			.attr("class","chartDayHeader")
+		.append('div')
+		
+	dayHeader.append('h3')
+		.text(day);
+}
+
+function createBarChart(config) {
+	var margin = {
+					top: config.margin.top, 
+					right: config.margin.right, 
+					bottom: config.margin.bottom, 
+					left: config.margin.left},
+	    width = config.width - margin.left - margin.right,
+	    height = config.height - margin.top - margin.bottom;
+
+	var parseDate = d3.time.format("%Y-%m-%d").parse;
+
+	var x = d3.scale.linear()
+		.domain([0,config.data.length])
+	    .range([0, width]);
+
+	var y = d3.scale.linear()
+		.domain([0,userConfiguration.week.maxValue])
+	    .range([0, height-5]);
+
+	var xAxis = d3.svg.axis()
+	    .scale(x)
+	    .orient("bottom");
+
+	// var yAxis = d3.svg.axis()
+	//     .scale(y)
+	//     .orient("left");
+	var yAxis = d3.svg.axis().scale(y).ticks(4).orient("left");
+
+	getDayHeadersForWeeklyCharts(config.target,config.id,config.className,config.day);
+		
+	d3.select(config.target+" #"+config.id)
+		.append('div')
+			.attr("class","chartDayArea");
+
+	var barWidth = (width / config.data.length);
+
+	var svg = d3.select("#"+config.id+" .chartDayArea").append("svg")
+				    .attr("width", width + margin.left + margin.right)
+				    .attr("height", height + margin.top + margin.bottom);
+	
+	// svg.append("g")
+	// 	.attr("class", "y axis")
+	// 	.attr("transform", "translate(0,0)")
+	// 	.call(yAxis);
+
+	svg.append("g")
+		.attr("class", "y axis")
+		.call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0)
+      // .attr("dy", ".71em")		
+      // .style("text-anchor", "end")
+      // .text("Price ($)");
+
+	svg.selectAll('rect')
+		.data(config.data)
+		.enter()
+		.append("rect")
+		.attr({
+			'width': (barWidth-.5),
+			'height': function(d) {
+				return 	y(d);
+			},
+			'x': function(d,i) {
+				// if (d>0)
+				// 	console.log("D: " + i+ "  | s: "+d);
+				return (i*barWidth)+.5;
+			},
+			'y': function(d,i) {
+				return height - y(d);
+			}
+		});
+
+	
+}
+
+function getMax(data) {
+	// var max = Math.max.apply(Math,data);
+	// console.log(max); 
+	return Math.max.apply(Math,data);
+}
+
 
